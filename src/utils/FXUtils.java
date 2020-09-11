a8f4d68c711457e786e3dca5570eb5e3fbbc0e97
Synergetic00 <evanjgillard@gmail.com>
2020-09-11 22:59:16 +1000

    V0.2.2
    Changed to doubles so function syntax as default in Processing is float not double, so using doubles now to allow for no "f"s in functions. Add more too.

--- src/utils/FXUtils.java
+++ src/utils/FXUtils.java
@@ -16,19 +16,10 @@ public class FXUtils {
         return (a < 0) ? -a : a;
     }
 
-    public static float abs(float a) {
-        return (a <= 0.0F) ? 0.0F - a : a;
-    }
-
     public static double abs(double a) {
-        return (a <= 0.0D) ? 0.0D - a : a;
+        return (a <= 0.0) ? 0.0 - a : a;
     }
     
-    public static int ceil(float val) {
-        if (val % 1 == 0) return (int) val;
-        return (int) ((val > 0) ? val - (val % 1) + 1: val - (val % 1) );
-    }
-
     public static int ceil(double val) {
         if (val % 1 == 0) return (int) val;
         return (int) ((val > 0) ? val - (val % 1) + 1: val - (val % 1) );
@@ -38,56 +29,56 @@ public class FXUtils {
 		return (val < low) ? low : ((val > high) ? high : val);
     }
     
-    public static float constrain(double val, double low, double high) {
-		return (float) ((val < low) ? low : ((val > high) ? high : val));
+    public static double constrain(double val, double low, double high) {
+		return ((val < low) ? low : ((val > high) ? high : val));
     }
     
-    public static float dist(double x1, double y1, double x2, double y2) {
-        return (float) Math.sqrt(abs(x2 - x1) * abs(x2 - x1) + abs(y2 - y1) * abs(y2 - y1));
+    public static double dist(double x1, double y1, double x2, double y2) {
+        return Math.sqrt(sq(abs(x1 - x2)) + sq(abs(y1 - y2)));
+    }
+    
+    public static double dist(double x1, double y1, double z1, double x2, double y2, double z2) {
+        return Math.sqrt(sq(abs(x1 - x2)) + sq(abs(y1 - y2)) + sq(abs(z1 - z2)));
     }
 
-    public static float exp(double n) {
-        return (float) Math.exp(n);
-    }
-
-    public static int floor(float val) {
-		return (int) ((val > 0) ? val - (val % 1) : val - (val % 1) - 1) ;
+    public static double exp(double n) {
+        return Math.exp(n);
     }
 
     public static int floor(double val) {
 		return (int) ((val > 0) ? val - (val % 1) : val - (val % 1) - 1) ;
     }
 
-    public static float lerp(double start, double stop, double amt) {
-        return (float) (start + amt * (stop - amt));
+    public static double lerp(double start, double stop, double amt) {
+        return (start + amt * (stop - amt));
     }
 
-    public static float log(double n) {
-        return (float) Math.log(n);
+    public static double log(double n) {
+        return Math.log(n);
     }
 
-    public static float mag(double x, double y) {
+    public static double mag(double x, double y) {
         return dist(0, 0, x, y);
     }
 
-    public static float map(double value, double start1, double stop1, double start2, double stop2) {
-        return (float) ((1d/((stop1-start1)/(stop2-start2)))*(value-stop1)+stop2);
+    public static double map(double value, double start1, double stop1, double start2, double stop2) {
+        return ((1d/((stop1-start1)/(stop2-start2)))*(value-stop1)+stop2);
     }
 
     public static int max(int a, int b) {
         return (a > b) ? a : b;
     }
 
-    public static float max(double a, double b) {
-        return (float) ((a > b) ? a : b);
+    public static double max(double a, double b) {
+        return ((a > b) ? a : b);
     }
 
     public static int max(int a, int b, int c) {
         return (a > b) ? ((a > c) ? a : c) : (b > c) ? b : c;
     }
 
-    public static float max(double a, double b, double c) {
-        return (float) ((a > b) ? ((a > c) ? a : c) : (b > c) ? b : c);
+    public static double max(double a, double b, double c) {
+        return ((a > b) ? ((a > c) ? a : c) : (b > c) ? b : c);
     }
 
     public static int max(int[] arr) {
@@ -96,26 +87,26 @@ public class FXUtils {
         return max;
     }
 
-    public static float max(double[] arr) {
+    public static double max(double[] arr) {
         double max = arr[0];
         for (double i : arr) if (i > max) max = i;
-        return (float) max;
+        return max;
     }
 
     public static int min(int a, int b) {
         return (a < b) ? a : b;
     }
 
-    public static float min(double a, double b) {
-        return (float) ((a < b) ? a : b);
+    public static double min(double a, double b) {
+        return ((a < b) ? a : b);
     }
 
     public static int min(int a, int b, int c) {
         return (a < b) ? ((a < c) ? a : c) : (b < c) ? b : c;
     }
 
-    public static float min(double a, double b, double c) {
-        return (float) ((a < b) ? ((a < c) ? a : c) : (b < c) ? b : c);
+    public static double min(double a, double b, double c) {
+        return ((a < b) ? ((a < c) ? a : c) : (b < c) ? b : c);
     }
 
     public static int min(int[] arr) {
@@ -124,78 +115,78 @@ public class FXUtils {
         return min;
     }
 
-    public static float min(double[] arr) {
+    public static double min(double[] arr) {
         double min = arr[0];
         for (double i : arr) if (i < min) min = i;
-        return (float) min;
+        return min;
     }
 
-    public static float norm(double value, double start, double stop) {
-        return (float) (value / (stop - start));
+    public static double norm(double value, double start, double stop) {
+        return (value / (stop - start));
     }
 
-    public static float pow(double n, double e) {
-        return (float) Math.pow(n, e);
+    public static double pow(double n, double e) {
+        return Math.pow(n, e);
     }
 
-    public static float round(double a) {
-        return (float) Math.round(a);
+    public static double round(double a) {
+        return Math.round(a);
     }
 
-    public static float sq(double a) {
+    public static double sq(double a) {
         return pow(a,2);
     }
 
-    public static float sqrt(double a) {
-        return (float) Math.sqrt(a);
+    public static double sqrt(double a) {
+        return Math.sqrt(a);
     }
 
-    public static float acos(double a) {
-        return (float) Math.acos(a);
+    public static double acos(double a) {
+        return Math.acos(a);
     }
 
-    public static float asin(double a) {
-        return (float) Math.asin(a);
+    public static double asin(double a) {
+        return Math.asin(a);
     }
 
-    public static float atan(double a) {
-        return (float) Math.atan(a);
+    public static double atan(double a) {
+        return Math.atan(a);
     }
 
-    public static float atan2(double y, double x) {
-        return (float) Math.atan2(y, x);
+    public static double atan2(double y, double x) {
+        return Math.atan2(y, x);
     }
 
-    public static float cos(double a) {
-        return (float) Math.cos(a);
+    public static double cos(double a) {
+        return Math.cos(a);
     }
 
-    public static float degrees(double a) {
-        return (float) Math.toDegrees(a);
+    public static double degrees(double a) {
+        return Math.toDegrees(a);
     }
 
-    public static float radians(double a) {
-        return (float) Math.toRadians(a);
+    public static double radians(double a) {
+        return Math.toRadians(a);
     }
 
-    public static float sin(double a) {
-        return (float) Math.sin(a);
+    public static double sin(double a) {
+        return Math.sin(a);
     }
 
-    public static float tan(double a) {
-        return (float) Math.tan(a);
+    public static double tan(double a) {
+        return Math.tan(a);
     }
 
-    public static float random(double high) {
+    public static double random(double high) {
         return random(0, high);
     }
 
-    public static float random(double low, double high) {
-        return (float) (low + (high - low) * r.nextDouble());
+    public static double random(double low, double high) {
+        return (low + (high - low) * r.nextDouble());
     }
 
-    public static float randomGaussian() {
-        return (float) r.nextGaussian();
+    public static double randomGaussian() {
+        return r.nextGaussian();
     }
 
     public static void randomSeed(int seed) {
@@ -204,10 +195,10 @@ public class FXUtils {
     
     // Constants
 
-    public static final float HALF_PI = 1.5707964f;
-    public static final float PI = 3.1415927f;
-    public static final float QUARTER_PI = 0.7853982f;
-    public static final float TAU = 6.2831855f;
-    public static final float TWO_PI = 6.2831855f;
+    public static final double HALF_PI = 1.5707964f;
+    public static final double PI = 3.1415927f;
+    public static final double QUARTER_PI = 0.7853982f;
+    public static final double TAU = 6.2831855f;
+    public static final double TWO_PI = 6.2831855f;
     
 }
--- src/utils/PVector.java
+++ src/utils/PVector.java
@@ -1,29 +1,31 @@
 package utils;
 
+import static utils.FXUtils.*;
+
 public class PVector {
 
-    float x, y, z;
+    double x, y, z;
 
     public PVector(double x, double y) {
-        this.x = (float) x;
-        this.y = (float) y;
+        this.x = x;
+        this.y = y;
     }
 
     public PVector(double x, double y, double z) {
-        this.x = (float) x;
-        this.y = (float) y;
-        this.z = (float) z;
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
     public void set(double x, double y) {
-        this.x = (float) x;
-        this.y = (float) y;
+        this.x = x;
+        this.y = y;
     }
 
     public void set(double x, double y, double z) {
-        this.x = (float) x;
-        this.y = (float) y;
-        this.z = (float) z;
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
     public void set(PVector a) {
@@ -32,40 +34,170 @@ public class PVector {
         this.z = a.z;
     }
 
-    public void set(float[] arr) {
+    public void set(double[] arr) {
         if (arr.length < 2) return;
         this.x = arr[0];
         this.y = arr[1];
         if (arr.length == 3) this.z = arr[2];
     }
 
-    public float[] array() {
-        return new float[] {x, y, z};
+    public double mag() {
+        return sqrt(x*x + y*y + z*z);
+    }
+
+    public double magSq() {
+        return (x*x + y*y + z*z);
+    }
+
+    public void add(PVector v) {
+        this.x += v.x;
+        this.y += v.y;
+        this.z += v.z;
+    }
+
+    public void add(double x, double y) {
+        this.x += x;
+        this.y += y;
+    }
+
+    public void add(double x, double y, double z) {
+        this.x += x;
+        this.y += y;
+        this.z += z;
+    }
+
+    public static PVector add(PVector v1, PVector v2) {
+        return new PVector(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+    }
+
+    public static void add(PVector v1, PVector v2, PVector target) {
+        target.x = v1.x + v2.x;
+        target.y = v1.y + v2.y;
+        target.z = v1.z + v2.z;
+    }
+
+    public void sub(PVector v) {
+        this.x -= v.x;
+        this.y -= v.y;
+        this.z -= v.z;
+    }
+
+    public void sub(double x, double y) {
+        this.x -= x;
+        this.y -= y;
+    }
+
+    public void sub(double x, double y, double z) {
+        this.x -= x;
+        this.y -= y;
+        this.z -= z;
+    }
+
+    public static PVector sub(PVector v1, PVector v2) {
+        return new PVector(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+    }
+
+    public static void sub(PVector v1, PVector v2, PVector target) {
+        target.x = v1.x - v2.x;
+        target.y = v1.y - v2.y;
+        target.z = v1.z - v2.z;
+    }
+
+    public void mult(double n) {
+        this.x *= n;
+        this.y *= n;
+        this.z *= n;
+    }
+
+    public static PVector mult(PVector v, double n) {
+        return new PVector(v.x * n, v.y * n, v.z * n);
+    }
+
+    public static void mult(PVector v, double n, PVector target) {
+        target.x = v.x * n;
+        target.y = v.y * n;
+        target.z = v.z * n;
+    }
+
+    public void div(double n) {
+        this.x /= n;
+        this.y /= n;
+        this.z /= n;
+    }
+
+    public static PVector div(PVector v, double n) {
+        return new PVector(v.x / n, v.y / n, v.z / n);
+    }
+
+    public static void div(PVector v, double n, PVector target) {
+        target.x = v.x / n;
+        target.y = v.y / n;
+        target.z = v.z / n;
+    }
+
+    public double dist(PVector v) {
+        return dist(new PVector(0,0,0), v);
+    }
+
+    public double dist(PVector v1, PVector v2) {
+        return FXUtils.dist(v1.x, v1.y, v1.z, v2.x, v2.y, v2.z);
+    }
+
+    public double dot(PVector v) {
+        double dotProd = 0;
+        dotProd += x * v.x;
+        dotProd += y * v.y;
+        dotProd += z * v.z;
+        return dotProd;
+    }
+
+    public double dot(double x, double y, double z) {
+        
+    }
+
+    public double dot(PVector v1, PVector v2) {
+
+    }
+
+    public PVector cross(PVector v) {
+
+    }
+
+    public static PVector cross(PVector v, PVector target) {
+        
+    }
+
+    public static void cross(PVector v1, PVector v2, PVector target) {
+        
+    }
+
+    public double[] array() {
+        return new double[] {x, y, z};
     }
 
     /*	
-    set()	        Set the components of the vector
-    random2D()	    Make a new 2D unit vector with a random direction.
-    random3D()	    Make a new 3D unit vector with a random direction.
-    fromAngle()	    Make a new 2D unit vector from an angle
-    copy()	        Get a copy of the vector
-    mag()	        Calculate the magnitude of the vector
-    magSq()	        Calculate the magnitude of the vector, squared
-    add()	        Adds x, y, and z components to a vector, one vector to another, or two independent vectors
-    sub()	        Subtract x, y, and z components from a vector, one vector from another, or two independent vectors
-    mult()	        Multiply a vector by a scalar
-    div()	        Divide a vector by a scalar
-    dist()	        Calculate the distance between two points
-    dot()	        Calculate the dot product of two vectors
-    cross()	        Calculate and return the cross product
-    normalize()	    Normalize the vector to a length of 1
-    limit()	        Limit the magnitude of the vector
-    setMag()	    Set the magnitude of the vector
-    heading()	    Calculate the angle of rotation for this vector
-    rotate()	    Rotate the vector by an angle (2D only)
-    lerp()	        Linear interpolate the vector to another vector
-    angleBetween()	Calculate and return the angle between two vectors
-    array()	        Return a representation of the vector as a float array
+    ✔    set()	        Set the components of the vector
+        random2D()	    Make a new 2D unit vector with a random direction.
+        random3D()	    Make a new 3D unit vector with a random direction.
+        fromAngle()	    Make a new 2D unit vector from an angle
+        copy()	        Get a copy of the vector
+    ✔    mag()	        Calculate the magnitude of the vector
+    ✔    magSq()	        Calculate the magnitude of the vector, squared
+    ✔    add()	        Adds x, y, and z components to a vector, one vector to another, or two independent vectors
+    ✔    sub()	        Subtract x, y, and z components from a vector, one vector from another, or two independent vectors
+    ✔    mult()	        Multiply a vector by a scalar
+    ✔    div()	        Divide a vector by a scalar
+    ✔    dist()	        Calculate the distance between two points
+        dot()	        Calculate the dot product of two vectors
+        cross()	        Calculate and return the cross product
+        normalize()	    Normalize the vector to a length of 1
+        limit()	        Limit the magnitude of the vector
+        setMag()	    Set the magnitude of the vector
+        heading()	    Calculate the angle of rotation for this vector
+        rotate()	    Rotate the vector by an angle (2D only)
+        lerp()	        Linear interpolate the vector to another vector
+        angleBetween()	Calculate and return the angle between two vectors
+    ✔    array()	        Return a representation of the vector as a double array
     */
 
 }
