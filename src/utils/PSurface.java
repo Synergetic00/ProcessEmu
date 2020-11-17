6c8dc3a630cdc17b5fea9239167f457a6a09f3c8
Synergetic00 <synergetic00@gmail.com>
2020-11-17 21:20:00 +1100

    V2.2.5

---
+++ src/utils/PSurface.java
@@ -0,0 +1,17 @@
+package utils;
+
+public class PSurface {
+
+    public void setLocation(int x, int y) {
+        
+    }
+
+    public void setResizable(boolean resizable) {
+        
+    }
+
+    public void setTitle(String title) {
+        
+    }
+    
+}
